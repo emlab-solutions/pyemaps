1126a6898a0956a67f9fbf7895dc592c4d0d6f95
Sharon Zhu <xiurongzhu@gmail.com>
2022-04-24 11:08:15 -0500

    initial checkin for pyemaps project - a fortran to python conversion and publish project

---
+++ __init__.py
@@ -0,0 +1,24 @@
+"""
+pyemaps package: 
+This python package contains python modules that simulates
+diffraction over selected crystal:
+
+   1) Dif: Kinematic Diffraction Simulation (free, MIT license)
+   2) Bloch: Dynamic Bloch Diffraction Simulation (proprietory, license needed)
+   3) crystals: python modules for cystal data class: Crystal
+
+All of the above python modules are based on Fortran application.
+
+More modules are developed in the future.
+
+Usage:
+    from pyemaps import dif
+    from pyemaps import crystal as cryst
+
+
+"""
+from pyemaps import __config__
+
+from .diffract import dif
+from .crystals import Crystal
+from .models import diffPattern as DP
