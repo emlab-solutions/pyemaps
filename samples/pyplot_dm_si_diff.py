ccc237a90f28a7f1df88d90b2451f064978237da
Sharon Zhu <xiurongzhu@gmail.com>
2022-05-07 14:18:03 -0500

    more comments header for each sample code

--- samples/pyplot_dm_si_diff.py
+++ samples/pyplot_dm_si_diff.py
@@ -1,29 +1,28 @@
 """
-# This file is part of pyemaps
-# ___________________________
-#
-# pyemaps is free software for non-comercial use: you can 
-# redistribute it and/or modify it under the terms of the GNU General 
-# Public License as published by the Free Software Foundation, either 
-# version 3 of the License, or (at your option) any later version.
-#
-# pyemaps is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
-#
-# Contact supprort@emlabsoftware.com for any questions and comments.
-# ___________________________
+This file is part of pyemaps
+___________________________
 
-An example of using pyemaps crystal and diffraction modules to 
-1) create a crystal from built-in data for Silicon 
-2) generate kinematical diffraction patterns
-3) display the diffraction pattern using pyemaps's built-in plot function 
+pyemaps is free software for non-comercial use: you can 
+redistribute it and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
 
-See https://emlab-solutions.github.io/pyemaps/ for pemaps usage
+pyemaps is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. see 
+<https://www.gnu.org/licenses/>.
+
+For how to install pyemaps, go to https://emlab-solutions.github.io/pyemaps/ 
+Contact supprort@emlabsoftware.com for any questions and comments.
+___________________________
+
+This sample code demonstrates how pyemaps can be integrated into 
+DigitalMicrograph to render the diffraction patterns for silicon crystal.
+
+Author:     EMLab Solutions, Inc.
+Date:       May 07, 2022    
+
 """
 from pyemaps import DEF_CONTROLS
 import sys
--- samples/sample_base.py
+++ samples/sample_base.py
@@ -1,27 +1,32 @@
 """
-# This file is part of pyemaps
-# ___________________________
-#
-# pyemaps is free software for non-comercial use: you can 
-# redistribute it and/or modify it under the terms of the GNU General 
-# Public License as published by the Free Software Foundation, either 
-# version 3 of the License, or (at your option) any later version.
-#
-# pyemaps is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
-#
-# Contact supprort@emlabsoftware.com for any questions and comments.
-# ___________________________
+This file is part of pyemaps
+___________________________
 
-Sample code to demostrate using pyemaps to generate kinematic diffraction patterns chaning with
-sample tilt in x direction between range of
+pyemaps is free software for non-comercial use: you can 
+redistribute it and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
+
+pyemaps is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact supprort@emlabsoftware.com for any questions and comments.
+___________________________
+
+This sample code is to demostrate using pyemaps to generate 
+kinematic diffraction patterns chaning with sample tilt in x direction 
+between range of
     (-1,1)
 with step of 0.5
+
+Author:     EMLab Solutions, Inc.
+Date:       May 07, 2022    
+
 """
 MAX_PROCWORKERS = 4
 
