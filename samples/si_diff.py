241ab13029e6128184df605bfecb9398cab73111
Sharon Zhu <xiurongzhu@gmail.com>
2022-05-07 14:18:03 -0500

    more comments header for each sample code

--- samples/si_diff.py
+++ samples/si_diff.py
@@ -1,22 +1,22 @@
 """
-# This file is part of pyemaps
-# ___________________________
-#
-# pyemaps is free software for non-comercial use: you can 
-# redistribute it and/or modify it under the terms of the GNU General 
-# Public License as published by the Free Software Foundation, either 
-# version 3 of the License, or (at your option) any later version.
-#
-# pyemaps is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
+This file is part of pyemaps
+___________________________
 
-# Contact supprort@emlabsoftware.com for any questions and comments.
-# ___________________________
+pyemaps is free software for non-comercial use: you can 
+redistribute it and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
+
+pyemaps is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact supprort@emlabsoftware.com for any questions and comments.
+___________________________
 
 An example of using pyemaps crystal and diffraction modules to 
 1) create a crystal from built-in data for Silicon 
@@ -24,6 +24,9 @@ An example of using pyemaps crystal and diffraction modules to
 3) display the diffraction pattern using pyemaps's built-in plot function 
 
 See https://emlab-solutions.github.io/pyemaps/ for pemaps usage
+
+Author:     EMLab Solutions, Inc.
+Date:       May 07, 2022    
 
 """
 
