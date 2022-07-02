b1c0769eb29f792a8ef71cdb738dcfb5ae0ca6f8
Sharon Zhu <xiurongzhu@gmail.com>
2022-07-01 21:42:24 -0500

    bug fixes for printCSF EMC and sanity test streamlining etc

--- samples/sample_base.py
+++ samples/sample_base.py
@@ -31,36 +31,9 @@ from pickle import EMPTY_DICT
 from pyemaps import DEF_CBED_DSIZE, DEF_MODE
 from pyemaps import EMC, DPError, DPListError, EMCError
 
-# import copy
-
 MAX_PROCWORKERS = 4
-def genDP(cr=None, dsize = None, mode = DEF_MODE, em_dict=EMPTY_DICT):
 
-    if not isinstance(em_dict, dict) and len(em_dict) != 1: 
-        raise DPError("Control arguement incorrect")
-
-    emc = EMC()
-    
-    for k, v in em_dict.items():
-        if k == 'tilt':
-            emc.tilt = v
-
-        if k == 'zone':
-            emc.zone = v
-
-        if k == 'defl':
-            emc.defl = v
-        
-        if k == 'cl':
-            emc.cl = v
-            
-        if k == 'vt':
-            emc.vt = v
-
-    return cr.generateDP(mode=mode, dsize=dsize, em_controls = emc)
-
-
-def generate_difs(name = 'silicon', mode = DEF_MODE):
+def generate_difs(name = 'silicon', mode = DEF_MODE, ckey = 'tilt'):
     from pyemaps import DPList
     import concurrent.futures
     from pyemaps import Crystal as cryst
@@ -77,150 +50,38 @@ def generate_difs(name = 'silicon', mode = DEF_MODE):
     # create an empty diffraction pattern list
     difs = DPList(name, mode = mode)
 
+    emclist =[] 
+
+    for i in range(-3,3): 
+
+        if ckey == 'tilt':
+            emclist.append(EMC(tilt=(i*0.5, 0.0)))
+        
+        if ckey == 'zone':
+            emclist.append(EMC(zone=(0, i, 1)))
+
+        if ckey == 'defl':
+            emclist.append(EMC(defl=(i*0.5, 0.0)))
+
+        if ckey == 'vt':
+            emclist.append(EMC(vt=200 + i*10))
+
+        if ckey == 'cl':
+            emclist.append(EMC(cl=1000 + i*50))
+
     with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROCWORKERS) as e:
-        for i in range(-3,3):
-            third = dict(tilt = (i*0.5, 0.0))
-            fs.append(e.submit(genDP, cr=cr, dsize=dsize, mode=mode, em_dict=third))
+
+        for ec in emclist:
+            fs.append(e.submit(cr.generateDP, mode=mode,  dsize=dsize, em_controls = ec))
 
         for f in concurrent.futures.as_completed(fs):
             try:
                 emc, diffP = f.result()
                 difs.add(emc, diffP)
+                
             except (DPError, EMCError, DPListError) as e:
                 print(f'{f} generated an exception: {e.message}')
             except:
-                print('failed to generate doffraction patterns')
+                print('failed to generate diffraction patterns')
             
-    return difs
-
-def generate_difs_defl(name = 'silicon', mode = DEF_MODE):
-    from pyemaps import DPList
-    from pyemaps import Crystal as cryst
-    
-    import concurrent.futures
-
-    cr = cryst.from_builtin(name)
-
-    if mode == 2:
-        dsize = DEF_CBED_DSIZE
-    else:
-        dsize = None
-
-    
-    fs=[]
-    difs = DPList(cr.name, mode = mode)
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROCWORKERS) as e:
-        for i in range(-10,10):
-            third = dict(defl = (i*0.5, 0.0))
-            fs.append(e.submit(genDP, cr=cr, dsize=dsize, mode=mode, em_dict=third))
-
-        for f in concurrent.futures.as_completed(fs):
-           try:
-                emc, diffP = f.result()
-                difs.add(emc, diffP)
-           except (DPError, EMCError, DPListError) as e:
-                print(f'{f} generated an exception: {e.message}')
-           except:
-                print('failed to generate doffraction patterns')
-
-    return difs
-
-def generate_difs_zone(name = 'silicon', mode = DEF_MODE):
-    from pyemaps import DPList
-    from pyemaps import Crystal as cryst
-    
-    import concurrent.futures
-
-    cr = cryst.from_builtin(name)
-
-    if mode == 2:
-        dsize = DEF_CBED_DSIZE
-    else:
-        dsize = None
-
-    
-    fs=[]
-    difs = DPList(cr.name, mode = mode)
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROCWORKERS) as e:
-        for i in range(-3,3):
-            third = dict(zone = (0, i, 1))
-            fs.append(e.submit(genDP, cr=cr, dsize=dsize, mode=mode, em_dict=third))
-
-        for f in concurrent.futures.as_completed(fs):
-           try:
-                emc, diffP = f.result()
-                difs.add(emc, diffP)
-           except (DPError, EMCError, DPListError) as e:
-                print(f'{f} generated an exception: {e.message}')
-           except:
-                print('failed to generate doffraction patterns')
-
-    return difs
-
-def generate_difs_cl(name = 'silicon', mode = DEF_MODE):
-    from pyemaps import DPList
-    from pyemaps import Crystal as cryst
-    
-    
-    import concurrent.futures
-
-    cr = cryst.from_builtin(name)
-
-    if mode == 2:
-        dsize = DEF_CBED_DSIZE
-    else:
-        dsize = None
-
-    
-    fs=[]
-    difs = DPList(cr.name, mode = mode)
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROCWORKERS) as e:
-        for i in range(-3,3):
-            third = dict(cl = 1000 + i*50)
-            fs.append(e.submit(genDP, cr=cr, dsize=dsize, mode=mode, em_dict=third))
-
-        for f in concurrent.futures.as_completed(fs):
-           try:
-                emc, diffP = f.result()
-                difs.add(emc, diffP)
-           except (DPError, EMCError, DPListError) as e:
-                print(f'{f} generated an exception: {e.message}')
-           except:
-                print('failed to generate doffraction patterns')
-    return difs
-
-def generate_difs_vt(name = 'silicon', mode = DEF_MODE):
-    from pyemaps import DPList
-    from pyemaps import Crystal as cryst
-    # from pyemaps import dif 
-    
-    import concurrent.futures
-
-    cr = cryst.from_builtin(name)
-
-    if mode == 2:
-        dsize = DEF_CBED_DSIZE
-    else:
-        dsize = None
-
-    
-    fs=[]
-    difs = DPList(cr.name, mode = mode)
-    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROCWORKERS) as e:
-        for i in range(-3,3):
-            third = dict(vt = 200 + i*10)
-            fs.append(e.submit(genDP, cr=cr, dsize=dsize, mode=mode, em_dict=third))
-
-        for f in concurrent.futures.as_completed(fs):
-           try:
-                emc, diffP = f.result()
-                difs.add(emc, diffP)
-           except (DPError, EMCError, DPListError) as e:
-                print(f'{f} generated an exception: {e.message}')
-           except:
-                print('failed to generate doffraction patterns')
-
     return difs
