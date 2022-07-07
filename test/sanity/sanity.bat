f62d61ec76d4f1a5d6a49345cb6e03fea036dd6d
Sharon Zhu <xiurongzhu@gmail.com>
2022-07-07 17:30:44 -0500

    further cleanup samples code

--- test/sanity/sanity.bat
+++ test/sanity/sanity.bat
@@ -4,21 +4,18 @@ call python -m pyemaps -c
 call python -m pyemaps -v
 
 if "%1"=="local" (
-    python .\import_calc_sanity 
     echo n | call python -m pyemaps -cp
     call python .\si_csf.py
     call python .\powder.py
     call python .\si_dif.py
-    call python .\si_dif_cbed.py
     call python .\si_bloch.py
-    goto:eof
+    python .\import_calc_sanity 
 ) else (
-    python test\sanity\import_calc_sanity
     echo y | call python -m pyemaps -cp
     call python .\pyemaps_samples\si_csf.py
     call python .\pyemaps_samples\powder.py
     call python .\pyemaps_samples\si_dif.py
-    call python .\pyemaps_samples\si_dif_cbed.py
     call python .\pyemaps_samples\si_bloch.py
+    python test\sanity\import_calc_sanity
     rm -rf pyemaps_samples
 )
