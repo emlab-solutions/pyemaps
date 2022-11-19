531cc9e7b45b880fdc2b108a007a90a4f0fc83a9
Sharon Zhu <xiurongzhu@gmail.com>
2022-11-19 10:17:45 -0600

    documents addition to the new release version

---
+++ samples/deprecated/bt.py
@@ -0,0 +1,45 @@
+cname = 'Boron_Tetra'
+MAX_PROCWORKERS = 2
+
+def bt_bloch():
+    import concurrent.futures
+    from pyemaps import Crystal as cryst
+    from pyemaps import showBloch, EMC, SIMC, BlochError, EMCError
+
+    cr = cryst.from_builtin(cname)
+    
+    zlist = [(1, 0, 0), (1, 0, 1), (0, 0, 1)]
+    # zlist = [(0, 0, 1)]
+    gmax = 3.5
+    sgmin = 0.3
+    sgmax = 1.0
+    sth = (50, 1000, 50)
+
+    simc = SIMC(gmax = gmax, excitation=(sgmin, sgmax))
+
+    emclist = [EMC(cl=200, zone=z, simc=simc) for z in zlist]
+    
+    fs=[]
+    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROCWORKERS) as e:
+
+        for ec in emclist:
+            fs.append(e.submit(cr.generateBloch, sample_thickness = sth, 
+                      em_controls = ec, disk_size=0.05, bSave=True))
+        
+        for f in concurrent.futures.as_completed(fs):
+            try:
+               bimg = f.result()
+
+            except (BlochError, EMCError) as e:
+                print(f'generated an exception: {e.message}') 
+                return bimg
+            except Exception as e:
+                print(f'failed to generate diffraction patterns: {e}') 
+                return bimg
+            else: 
+                showBloch(bimg) 
+            
+
+if __name__ == "__main__":
+    
+    bt_bloch()
