db9e29a255ae655d6d693a5a17cfa0b6ade94fd5
Sharon Zhu <xiurongzhu@gmail.com>
2023-02-06 21:51:51 -0600

    refined bloch temp file name generation and build batch

--- local.bat
+++ local.bat
@@ -1,7 +1,68 @@
+@echo off
+
 @REM CALL C:\Apps\Intel\compiler\2022.1.0\env\vars.bat intel64
 @REM cd pyemaps
-python -m pip uninstall -y pyemaps
-python build_pyemaps -t -v 3.1.2
-@REM python -m pip uninstall pyemaps
-python -m pip install dist\pyemaps-3.1.2-cp37-cp37m-win_amd64.whl
-@REM python samples\si_scm.py
+@REM input arguement of 
+@REM "free"--------builds free package and 
+@REM "all"-------- builds builds full package that includes DPGEN component.
+
+set EMAPS_BTYPE=%1
+set PACKAGE_TYPE=%2
+
+if "%EMAPS_BTYPE%" == "" (
+    echo Usage: local.bat build_type package_type
+    echo        build_type: free or all
+    echo        package_type: test or prod
+    goto:eof
+)
+
+if "%EMAPS_BTYPE%" NEQ "free" (
+    if "%1" NEQ "all" (
+        echo Error: Build type incorrect, must be one of free or all
+        goto:eof
+    )
+)
+
+if "%PACKAGE_TYPE%" == "" (
+    echo Usage: local.bat build_type package_type
+    echo        build_type: free or all
+    echo        package_type: test or prod
+    goto:eof
+)
+
+if "%PACKAGE_TYPE%" NEQ "test" (
+    if "%PACKAGE_TYPE%" NEQ "prod" (
+        echo Error: Package type incorrect, must be one of test or prod
+        goto:eof
+    )
+)
+
+
+call python -m pip uninstall -y pyemaps
+
+if "%PACKAGE_TYPE%" == "test" (
+    call python build_pyemaps -t -v 3.1.2
+    call python -m pip install dist\pyemaps-3.1.2-cp37-cp37m-win_amd64.whl
+    @REM echo got here after test package build
+) else (
+    call python build_pyemaps
+    call python -m pip install dist\pyemaps-0.6.3-cp37-cp37m-win_amd64.whl
+    @REM echo got here after production package build
+)
+
+
+set typename=%EMAPS_BTYPE%
+if "%EMAPS_BTYPE%" == "all" (
+    set typename=full
+)
+set packname=%PACKAGE_TYPE%
+if "%PACKAGE_TYPE%" == "prod" (
+    set packname=production
+)
+echo.
+echo.
+echo ##############################################
+echo %typename% %packname% package build completed
+echo ##############################################
+echo.
+echo.
