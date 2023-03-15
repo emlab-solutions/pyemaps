5eb046226f824dbed54fac2beee3d12dfa460af4
Sharon Zhu <xiurongzhu@gmail.com>
2023-03-14 22:27:24 -0500

    Final adjustment before first preview feature release

--- samples/au_ediom.py
+++ samples/au_ediom.py
@@ -9,11 +9,11 @@ def getTestDBFN():
     # return os.path.join(db_home, 'dpdb', 'Gold_FCC-20230220194314.bin')
     return os.path.join(db_home, 'dpdb', 'Gold_FCC-20230313104014.bin')
 
-def getIMSFn():
-    db_home = os.getenv('PYEMAPS_DATA')
-    # return os.path.join(db_home, 'bloch', 'Silicon-20230123155711.im3')
-    return os.path.join(db_home, 'dpdb', 'ediom', 'ExampleDP_Au_0.im3')
-    # return os.path.join(db_home, 'dpdb', 'ediom', 'ExampleDP_Au_2.im3')
+# def getIMSFn():
+#     db_home = os.getenv('PYEMAPS_DATA')
+#     # return os.path.join(db_home, 'bloch', 'Silicon-20230123155711.im3')
+#     return os.path.join(db_home, 'dpdb', 'ediom', 'ExampleDP_Au_0.im3')
+#     # return os.path.join(db_home, 'dpdb', 'ediom', 'ExampleDP_Au_2.im3')
 
 def getXTemplateFn():
     db_home = os.getenv('PYEMAPS_DATA')
@@ -26,7 +26,8 @@ def getIMSFn():
     db_home = os.getenv('PYEMAPS_DATA')
     # return os.path.join(db_home, 'bloch', 'Silicon-20230123155711.im3')
     # return os.path.join(db_home, 'dpdb', 'ediom', 'ExampleDP_Au_0.im3')
-    return os.path.join(db_home, 'dpdb', 'ediom', 'ExampleDP_Au_2.im3')
+    return os.path.join(db_home, 'dpdb', 'ediom', 'ExampleDP_Au_1.img')
+    # return os.path.join(db_home, 'dpdb', 'ediom', 'ExampleDP_Au_2.im3')
 
 def test_ediom(cname='Gold_FCC', xifn=None):
     from pyemaps import Crystal as cr
@@ -45,14 +46,14 @@ def test_ediom(cname='Gold_FCC', xifn=None):
     
     # locate the experimental image file
     edc0=ediom.cvar.edc
-    edc0.set_center(65.0, 66.4)
+    edc0.set_center(65.34, 67.01)
 
     if xifn is None or not os.path.exists(xifn):
         print(f'Experimental image must be provided')
         return -1
 
     dbfn = getTestDBFN()
-    au.indexExpDP(dbfn, xifn, edcp=edc0, threshold=0.8525)
+    au.indexExpDP(dbfn, xifn, edcp=edc0, threshold=0.825)
 
 if __name__ == '__main__':
     xifn0 = getIMSFn()
