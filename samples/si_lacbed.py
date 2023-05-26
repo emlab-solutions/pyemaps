7efda7513d9ab957b906710bc27f0c76eb1c20f7
sharonz2006 <xiurongzhu@gmail.com>
2023-05-25 22:41:07 -0500

    added lacbed sample code and linux compatibilities plus bug fixes in crystal module

---
+++ samples/si_lacbed.py
@@ -0,0 +1,55 @@
+"""
+This file is part of pyemaps
+___________________________
+
+pyemaps is free software for non-comercial use: you can 
+redistribute it and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
+
+pyemaps is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact supprort@emlabsoftware.com for any questions and comments.
+___________________________
+
+This sample code is to demostrate using pyemaps to generate and render
+dynamic diffraction patterns while controls. 
+
+Author:     EMLab Solutions, Inc.
+Date:       May 25, 2023    
+
+"""
+
+def generate_lacbed_images(name = 'Silicon', bShow=False):
+    
+    from pyemaps import Crystal as cryst
+
+    cr = cryst.from_builtin(name)
+    
+    try:
+        imgs = cr.generateBloch( disk_size = 0.25,
+                            sampling = 10,
+                            nType = 1, 
+                            sample_thickness=(1000,1000,100),
+                            bSave=True)
+    except Exception as e:
+        print(f'Unable to obtain bloch image(s): {e}') 
+        return
+
+    if bShow:        
+        imgs.sort()
+        showBloch(imgs)
+
+
+from pyemaps import showBloch
+
+if __name__ == '__main__':
+    
+    generate_lacbed_images(bShow=True)
+        
