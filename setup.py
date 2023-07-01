f1a6a5addff685b0c50e8d6dc8695d884bfd0b95
sharon <xiurongzhu@gmail.com>
2023-06-30 22:01:26 -0500

    setup change for ediom compiler fit for docker

--- setup.py
+++ setup.py
@@ -544,8 +544,8 @@ pyemaps_ediom =  Extension(
             libraries               =[],
             define_macros           = pyemaps_build_defs,
             undef_macros            = pyemaps_build_undefs,
-            extra_link_args         =[],
-            swig_opts               =['-python']
+            extra_link_args         =[]
+            # swig_opts               =['-python']
 )
 
 def get_version(f):
