66527d2ba8ea4c5ceba9038f590417e582dc7dd5
sharonz2006 <xiurongzhu@gmail.com>
2023-11-08 19:45:29 -0600

    prepare legal header for open source

--- diffract/stereo_dec.py
+++ diffract/stereo_dec.py
@@ -1,3 +1,28 @@
+'''
+This file is part of pyemaps
+___________________________
+
+pyemaps is free software for non-comercial use: you can 
+redistribute it and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
+
+pyemaps is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact supprort@emlabsoftware.com for any questions and comments.
+___________________________
+
+
+Author:             EMLab Solutions, Inc.
+Date Created:       May 07, 2022  
+
+'''
 def add_stereo(target):
     def generateStereo(self, xa = (0,2,0), 
                             tilt=(0.0,0.0),
--- local.bat
+++ local.bat
@@ -1,3 +1,28 @@
+@REM # '''
+@REM # This file is part of pyemaps
+@REM # ___________________________
+
+@REM # pyemaps is free software for non-comercial use: you can 
+@REM # redistribute it and/or modify it under the terms of the GNU General 
+@REM # Public License as published by the Free Software Foundation, either 
+@REM # version 3 of the License, or (at your option) any later version.
+
+@REM # pyemaps is distributed in the hope that it will be useful,
+@REM # but WITHOUT ANY WARRANTY; without even the implied warranty of
+@REM # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@REM # GNU General Public License for more details.
+
+@REM # You should have received a copy of the GNU General Public License
+@REM # along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
+
+@REM # Contact supprort@emlabsoftware.com for any questions and comments.
+@REM # ___________________________
+
+
+@REM # Author:             EMLab Solutions, Inc.
+@REM # Date Created:       May 07, 2022  
+
+@REM # '''
 @echo off
 
 set PYPIREPO=%1
