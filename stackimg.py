3ef88948893d4b99ff4e7d11d3eeb71ff54b26f6
sharonz2006 <xiurongzhu@gmail.com>
2023-11-08 19:48:04 -0600

    more legal header for open source

--- stackimg.py
+++ stackimg.py
@@ -1,31 +1,29 @@
-#'''
-# This file is part of pyemaps
-# ___________________________
-
-# pyemaps is free software for non-comercial use: you can 
-# redistribute it and/or modify it under the terms of the GNU General 
-# Public License as published by the Free Software Foundation, either 
-# version 3 of the License, or (at your option) any later version.
-
-# pyemaps is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
-
-# Contact supprort@emlabsoftware.com for any questions and comments.
-# ___________________________
-
-# Author:     EMLab Solutions, Inc.
-# Date:       May 16, 2023   
-#'''
-
 '''
+This file is part of pyemaps
+___________________________
+
+pyemaps is free software for non-comercial use: you can 
+redistribute it and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
+
+pyemaps is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact supprort@emlabsoftware.com for any questions and comments.
+___________________________
+
+Author:     EMLab Solutions, Inc.
+Date:       May 16, 2023   
+
 EDIOM (Electron Diffraction Indexing and Orientation Mapping) module 
 in pyemaps contains a rich set of diffraction pattern search and recognition
-functions, as well as those for orientation mapping.
+functions. Future feature addition include orientation mapping.
 
 *StachImage* class is designed to interface with EDIOM functions to provide
 users easy access to the collection of EDIOM features. 
