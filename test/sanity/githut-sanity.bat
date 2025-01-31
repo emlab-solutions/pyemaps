9d4bfab27e562de9d8eb1d602463bc1560a03c2b
Sharon Zhu <xiurongzhu@gmail.com>
2025-01-30 18:43:04 -0600

    Update githut-sanity.bat

--- test/sanity/githut-sanity.bat
+++ test/sanity/githut-sanity.bat
@@ -5,30 +5,30 @@
 set startTime=%time%
 echo Script started at %startTime%
 
-call python -m pyemaps -s
-call python -m pyemaps -c
-call python -m pyemaps -v
+@REM call pyemaps -s
+call pyemaps -c
+call pyemaps -v
 
-echo y | call python -m pyemaps -cp
+echo y | call pyemaps -cp
 call python .\pyemaps_samples\si_csf.py
-call python .\pyemaps_samples\si_dif.py
-call python .\pyemaps_samples\si_bloch.py
-call python .\pyemaps_samples\si_lacbed.py
+@REM call python .\pyemaps_samples\si_dif.py
+@REM call python .\pyemaps_samples\si_bloch.py
+@REM call python .\pyemaps_samples\si_lacbed.py
 @REM call python .\pyemaps_samples\si_rawblochimgs.py
 call python .\pyemaps_samples\si_constructor.py
-call python .\pyemaps_samples\si_stereo.py
-call python .\pyemaps_samples\si_scm.py
-python test\sanity\feature_sanity.py
+@REM call python .\pyemaps_samples\si_stereo.py
+@REM call python .\pyemaps_samples\si_scm.py
+@REM python test\sanity\feature_sanity.py
 
 @REM performance test - all includes all types: dif, bloch and stereo
 call python test\sanity\run_perf_test.py -r all
 @REM unit test: EMC SIMC class basic function tests
 call python test\unittests\emc\test1.py
-call python test\unittests\kdif\sanity_doc.py
-call python test\unittests\bloch\si_bloch_docs.py
+@REM call python test\unittests\kdif\sanity_doc.py
+@REM call python test\unittests\bloch\si_bloch_docs.py
 call python test\unittests\bloch\disk_size.py
 
-call python .\pyemaps_samples\powder.py
+@REM call python .\pyemaps_samples\powder.py
 @REM echo before wrror level: %ERRORLEVEL%
 call python test\unittests\package_test\type_test.py
 @REM echo after wrror level: %ERRORLEVEL%
@@ -38,11 +38,11 @@ if %return_code%==1 (
     echo ##############free package testing completed: %return_code%
 ) else if %return_code%==2 (
     call python .\pyemaps_samples\al_dpgen.py
-    call python .\pyemaps_samples\al_ediom.py
+    @REM call python .\pyemaps_samples\al_ediom.py
     echo ##############Full package testing completed: %return_code%
 ) else if %return_code%==3 (
     call python .\pyemaps_samples\al_dpgen.py
-    call python .\pyemaps_samples\al_ediom.py
+    @REM call python .\pyemaps_samples\al_ediom.py
     echo ##############UIUC package testing completed: %return_code%
 ) else (
     echo ##############Unknown package testing error: %return_code%
