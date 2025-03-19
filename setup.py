5c93035fa3af78ff24b8d8200cd8e78263ca57dc
sharonz2006 <xiurongzhu@gmail.com>
2025-03-19 12:49:25 -0500

    version requirement change

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ os.environ["MSSdk"] = "1"
 from pathlib import Path
 
 install_requires = [
-            'emaps >= 1.0.7',
+            'emaps >= 1.0.8',
             'numpy >= 1.21.2',
             'matplotlib >= 3.2.1'
             ]
